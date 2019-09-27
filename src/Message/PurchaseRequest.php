fe23c637eb20cb585853511591696cc6c86efa42
Mark O'Keeffe <mark.okeeffe@digistorm.com.au>
2019-09-27 15:17:55 +1000

    Post multipart/form-data for PurchaseRequest

--- src/Message/PurchaseRequest.php
+++ src/Message/PurchaseRequest.php
@@ -20,11 +20,13 @@ class PurchaseRequest extends AbstractRequest implements RequestInterface
         $expiryYear = substr($this->getCard()->getExpiryYear(), -2);
 
         return new MultipartStream([
-            'CardNumber' => $this->getCard()->getNumber(),
-            'ExpiryMonth' => $expiryMonth,
-            'ExpiryYear' => $expiryYear,
-            'CardHolderName' => $this->getCard()->getName(),
-            'Cvc2' => $this->getCard()->getCvv(),
+            [
+                'CardNumber' => $this->getCard()->getNumber(),
+                'ExpiryMonth' => $expiryMonth,
+                'ExpiryYear' => $expiryYear,
+                'CardHolderName' => $this->getCard()->getName(),
+                'Cvc2' => $this->getCard()->getCvv(),
+            ],
         ]);
     }
 
